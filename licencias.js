174839e53985b578cac8cd64b9e15f741c9de2c6
unknown <oemig.jose@osmgp.gov.ar>
2021-05-11 11:29:16 -0300

    Creamos el Index en Repo

--- licencias.js
+++ licencias.js
@@ -1,7 +1,3 @@
 console.log("Carga de Licencias");
 console.log("Ingrese su ci: ");
-<<<<<<< HEAD
-console.log("Modifico MAster ");
-=======
 console.log("Modifico Rama miOSSEgit add .: ");
->>>>>>> miOSSE
